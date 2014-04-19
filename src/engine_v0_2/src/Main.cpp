38f7173594d5c41ed4d70ee58bbb6c2267fcd677
maxwell <maxwell@eva-04>
2014-04-19 16:45:30 +0200

    Main

---
+++ src/engine_v0_2/src/Main.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Main.cpp
+ * Author: maxwell
+ *
+ * Created on 19 avril 2014, 16:25
+ */
+
+#include <cstdlib>
+#include <time.h>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    srand(time(NULL));
+    
+    return 0;
+}
+
